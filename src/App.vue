ee404368c27e379d801336cd99689015af0e9a72
CrabMan <tobecrabman@163.com>
2024-12-11 17:00:10 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello Vue.js!</h1>
+    <button @click="sendMessage">Send Message to Flutter</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    sendMessage() {
+      if (window.Toaster) {
+        window.Toaster.postMessage("Hello from Vue.js");
+      } else {
+        console.error("sendMessageToFlutter is not defined");
+      }
+    },
+  },
+};
 </script>
 
 <style>
